75b2660d6ece902852864a26274280f25284a946
Yanxing Wang <yanxing@uber.com>
2016-10-26 22:10:41 -0700

    add stars route

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import {Route} from 'react-router'
 import App from "./containers/App";
 import Login from "./containers/Login";
 import NotFound from "./components/NotFound";
+import Stars from "./components/Stars";
 
 export default(
   <Route path='/' component={App}>
     <Route path="login" component={Login}/>
+    <Route path="stars" component={Stars}/>
     <Route path="*" component={NotFound} />
   </Route>
 );
